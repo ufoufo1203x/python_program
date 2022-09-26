cb17d03984a01429d3c724e8a7336230d84d9537
ufoufo1203 <shineshine0425@gmail.com>
2022-09-26 23:57:43 +0900

    0927

---
+++ use_logging.py
@@ -0,0 +1,16 @@
+from random import randint
+import logging
+logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(asctime)s - %(levelname)s - %(message)s")
+
+def main():
+    counter = 0
+    while counter < 10:
+        number = randint(1, 13)
+        print("{0}回目は{1}。".format(counter+1, number))
+        logging.debug("counter={0}, numebr={1}".format(counter, number))
+        counter += 1
+        
+    if __name__ == '__main__':
+        logging.debug("Program start")
+        main()
+        logging.debug("Program end")
