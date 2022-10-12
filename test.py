3f8f96f472472d3e2889e404b570d5ad813c9763
ufoufo1203 <shineshine0425@gmail.com>
2022-10-12 12:36:44 +0900

    1012

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-a = abs(-1)
-print(a)
+list = [180, 170, 160, 165, 175]
+total = 0
+
+for i in list:
+    total = total + i
+    continue
+
+print("Total is",total)
+print("Avereage is",total/len(list))
